2062946bec6490793a902206344bee794b5d3727
ShushniK <aharon.yarin19@gmail.com>
2024-03-13 21:03:11 +0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let x = 1;
+  console.log("App Executed");
+
+  return (
+    <View style={styles.container}>
+      <Text> App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
